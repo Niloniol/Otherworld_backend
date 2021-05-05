241863bc04833c30ddb18467d38d11bf2f4011f9
Niloniol <hellorock@mail.ru>
2021-05-05 18:52:28 +0700

    initial commit

---
+++ src/main/java/ru/other_world/OtherWorldApplication.java
@@ -0,0 +1,13 @@
+package ru.other_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/other_world/OtherWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.other_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
